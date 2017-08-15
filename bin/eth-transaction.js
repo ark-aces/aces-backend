4c468992de4dcd5d576a3cdc6a21a57e005d16cc
Lee Marlow <imtheoneandonlyorval@gmail.com>
2017-08-14 17:37:33 -0700

    Added eth-transaction.js.

---
+++ bin/eth-transaction.js
@@ -0,0 +1,50 @@
+/*
+Usage:
+nodejs eth-transaction.js {codeBinary}
+
+Returns:
+Json object containing transactionHash
+
+Example:
+TODO: Add example
+
+Result:
+{"transactionHash": "..."}
+*/
+if (process.argv.length <= 6) {
+    console.log("Usage: " + __filename + " {{ethServiceUrl}} {{walletAddress}} {{walletPassword}} {{receiverAddress}} {{amount}}");
+    process.exit(-1);
+}
+
+let Web3 = require('web3');
+let ethServerUrl = process.argv[2];
+let walletAddress = process.argv[3];
+let walletPassword = process.argv[4];
+let receiverAddress = process.argv[5];
+let amount = process.argv[6];
+
+let web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider(ethServerUrl));
+
+// Unlock the sender's account
+web3.personal.unlockAccount(walletAddress, walletPassword);
+amount = web3.toWei(amount, "ether");
+
+// Send transaction
+let transaction = web3.eth.sendTransaction(
+    {
+        from: walletAddress,
+        to: receiverAddress,
+        value: amount
+    },
+    function(err, transactionHash) {
+        if (err) {
+            console.log(err);
+            process.exit(-1);
+        } else if (transactionHash) {
+            console.log(JSON.stringify({
+                "transactionHash": transactionHash,
+            }));
+        }
+    }
+);
--- src/main/java/io/ark/ark_client/EthTransactionResult.java
+++ src/main/java/io/ark/ark_client/EthTransactionResult.java
@@ -2,24 +2,7 @@ package io.ark.ark_client;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class EthTransactionResult {
-    private String id;
-    private Integer height;
-    private String blockId;
-    private Integer type;
-    private Integer timestamp;
-    private String senderPublicKey;
-    private String senderId;
-    private String recipientId;
-    private Long amount;
-    private Long fee;
-    private String signature;
-    private List<String> signatures;
-    private Integer confirmations;
-    private String vendorField;
-    private Object asset;
     private String transactionHash;
 }
